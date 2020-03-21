13462d87e07049f51c126a6c364f76cfb36c2ff5
Quisel <thomasdeimel@gmx.de>
2020-03-21 10:22:50 +0100

    ascending sorting fix

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserToken } from './entities/user-token.entity';
 import { UserRepository } from './repositories/user.repository';
+import { UserToken } from './entities/user-token.entity';
 import { ItemCategory } from './entities/itemCategory.entity';
 import { ShoppingList } from './entities/shoppinglist.entity';
 import { ShoppingListItem } from './entities/shoppingListItem.entity';
@@ -11,12 +11,12 @@ import { Item } from './entities/item.entity';
 @Module({
   imports: [
     TypeOrmModule.forFeature([
-      UserRepository,
-      UserToken,
       Item,
       ItemCategory,
       ShoppingList,
-      ShoppingListItem
+      ShoppingListItem,
+      UserRepository,
+      UserToken
     ])
   ],
   providers: [DatabaseService],
--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,18 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Repository, Connection } from 'typeorm';
-import { UserToken } from './entities/user-token.entity';
-import { UserRepository } from './repositories/user.repository';
-import { ItemCategory } from './entities/itemCategory.entity';
 import { Item } from './entities/item.entity';
+import { ItemCategory } from './entities/itemCategory.entity';
 import { ShoppingList } from './entities/shoppinglist.entity';
 import { ShoppingListItem } from './entities/shoppingListItem.entity';
+import { UserRepository } from './repositories/user.repository';
+import { UserToken } from './entities/user-token.entity';
 
 @Injectable()
 export class DatabaseService {
-    private readonly _userRepository: UserRepository;
-
-    private readonly _userTokenRepository: Repository<UserToken>;
-
     private readonly _itemCategoryRepository: Repository<ItemCategory>
 
     private readonly _itemRepository: Repository<Item>
@@ -21,23 +17,19 @@ export class DatabaseService {
 
     private readonly _shoppingListItemRepository: Repository<ShoppingListItem>
 
+    private readonly _userRepository: UserRepository;
+
+    private readonly _userTokenRepository: Repository<UserToken>;
+
     constructor(
         private readonly connection: Connection
     ) {
-        this._userRepository = this.connection.getCustomRepository(UserRepository);
-        this._userTokenRepository = this.connection.getRepository(UserToken);
         this._itemRepository = this.connection.getRepository(Item);
         this._itemCategoryRepository = this.connection.getRepository(ItemCategory);
         this._shoppingListItemRepository = this.connection.getRepository(ShoppingListItem);
         this._shoppingListRepository = this.connection.getRepository(ShoppingList);
-    }
-
-    public get userRepository(): UserRepository {
-        return this._userRepository;
-    }
-
-    public get userTokenRepository(): Repository<UserToken> {
-        return this._userTokenRepository;
+        this._userRepository = this.connection.getCustomRepository(UserRepository);
+        this._userTokenRepository = this.connection.getRepository(UserToken);
     }
 
     public get itemRepository(): Repository<Item> {
@@ -54,5 +46,13 @@ export class DatabaseService {
 
     public get shoppingListRepository(): Repository<ShoppingList> {
         return this._shoppingListRepository;
+    }
+
+    public get userRepository(): UserRepository {
+        return this._userRepository;
+    }
+
+    public get userTokenRepository(): Repository<UserToken> {
+        return this._userTokenRepository;
     }
 }
