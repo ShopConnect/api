fb6a1937c84eb05079b09ff500d076c6a2d4f0b9
Nick Strohm <nick.strohm99@gmail.com>
2020-03-21 01:26:18 +0100

    Add missing auth endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Body, Post } from '@nestjs/common';
-import { ApiTags, ApiForbiddenResponse, ApiBadRequestResponse, ApiNotFoundResponse } from '@nestjs/swagger';
+import { Controller, Body, Post, Get, UseGuards, Req } from '@nestjs/common';
+import { ApiTags, ApiBadRequestResponse, ApiOkResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { RegisterRequestDto } from '../_dtos/register-request.dto';
 import { RegisterResponseDto } from '../_dtos/register-response.dto';
+import { LoginRequestDto } from 'src/_dtos/login-request.dto';
+import { LoginResponseDto } from 'src/_dtos/login-response.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { User } from 'src/database/entities/user.entity';
+import { Request } from 'express';
 
 @Controller('auth')
 @ApiTags('auth')
@@ -13,7 +18,33 @@ export class AuthController {
 
     @Post('register')
     @ApiBadRequestResponse({ description: 'Username is already in use' })
+    @ApiOkResponse({ description: 'User is registered' })
     public register(@Body() registerRequest: RegisterRequestDto): Promise<RegisterResponseDto> {
         return this.authService.register(registerRequest);
+    }
+
+    @Post('login')
+    public login(@Body() loginRequest: LoginRequestDto): Promise<LoginResponseDto> {
+        return this.authService.login(loginRequest);
+    }
+    
+    @Get('logout')
+    @UseGuards(JwtAuthGuard)
+    public async logout(@Req() request: Request): Promise<void> {
+        const user = <User>request.user;
+        if (!user) {
+            return;
+        }
+
+        if (!request.headers.authorization) {
+            return;
+        }
+
+        const token = request.headers.authorization.split(' ', 2)[1];
+        if (!token) {
+            return;
+        }
+
+        await this.authService.logout(user, token);
     }
 }
