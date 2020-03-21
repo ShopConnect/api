0fd86e687bff2958c4d3e39b6ee22c5005367022
Nick Strohm <nick.strohm99@gmail.com>
2020-03-21 01:38:55 +0100

    Specify response properties

--- src/_dtos/login-response.dto.ts
+++ src/_dtos/login-response.dto.ts
@@ -1,9 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class LoginResponseDto {
+    @ApiProperty()
+    public readonly token: string;
+
     /**
      * Creates the login response model
      * @param token Access token for the authenticated user
      */
     constructor(
-        public readonly token: string,
-    ) { }
+        token: string,
+    ) {
+        this.token = token;
+    }
 }
--- src/_dtos/register-response.dto.ts
+++ src/_dtos/register-response.dto.ts
@@ -1,9 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class RegisterResponseDto {
+    @ApiProperty()
+    public readonly success: boolean
+
     /**
      * Creates the register response model
      * @param success Indicates wether the user was successfully registered
      */
     constructor(
-        public readonly success: boolean
-    ) { }
+        success: boolean
+    ) {
+        this.success = success;
+    }
 }
